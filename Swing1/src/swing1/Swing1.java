b8d07ec81c2903fc9db69035c8f1b02766cc6603
salsabilavebi <61673247+salsabilavebi@users.noreply.github.com>
2020-03-23 19:35:46 +0700

    Add files via upload

---
+++ Swing1/src/swing1/Swing1.java
@@ -0,0 +1,15 @@
+
+package swing1;
+
+
+public class Swing1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        FrmUtama utama = new FrmUtama ();
+        utama.setVisible(true);
+    }
+    
+}
